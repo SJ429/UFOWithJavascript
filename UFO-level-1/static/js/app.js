69da0d15dc328e9a33686ca19986ea7e6b027148
Susan <sjessopb89@gmail.com>
2020-08-20 16:28:28 -0400

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,19 +4,7 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody =d3.select ('tbody');
 console.log(data)
-//Loop Through data and console.log data
-data.forEach(function(tableData) {
-console.log(tableData);
- });
-//Use d3 to append table row 'tr'
-data.forEach(function(tableData) {
-console.log(tableData);
-var row = tbody.append('tr');
- });
-//Use object.entries to console.log
-Object.entries(tableData).forEach(function([key, value]) {
- console.log(key, value);
-});
+// //Loop Through data and console.log data
 // Use d3 to append row and data
 data.forEach(function(tableData) {
   console.log(tableData);
@@ -28,25 +16,31 @@ data.forEach(function(tableData) {
   });
 });
 
-
-// Listen to events and filter 
+// Listen to event and filter 
 var button = d3.select('#filter-btn');
 var form = d3.select('#filters');
 button.on('click', getStats);
 form.on('submit',getStats);
 function getStats() {
  
+  // Prevent the page from refreshing
   d3.event.preventDefault();
 
+ //seltect the imput element and get the HTML mode
   var inputElement =d3.select('#datetime');
+
+  //Get the valu property of the input element
   var inputValue = inputElement.property('value');
+
   console.log(inputValue);  
   console.log(tableData);
 
+  // Set up filter to for Data
   var filteredData = tableData.filter(date=> date.datetime ===inputValue);
   console.log(filteredData);
   d3.select('tbody').html('');
 
+  // Append and filter data
   filteredData.forEach(function(tableData) {
     console.log(tableData);
     var row = tbody.append('tr');
