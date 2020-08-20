d485707c2180d945c206eaf9343c4f4f958c87e9
Susan <sjessopb89@gmail.com>
2020-08-19 20:00:09 -0400

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -30,21 +30,4 @@ data.forEach(function(tableData) {
 
 
 
-   // Use D3 to select the table
-// var table = d3.select('table');
-// //Use D3 to set the table class to 'table table-stripe'
-// table.attr('class', 'table table-dark');
-
-// var thead = d3.select('thead');
-// row_h = thead.append('tr');
-// Object.keys(tableData[0]).forEach((key)=>
-// {
-// var cell=row_h.append('th');
-// data.forEach((tableData)=> {
-// var row =tbody.append('tr');
-// Object.values(tableData).forEach((value)=>{
-// var cell = row.append('td');
-// cell.text(value);  
-
-//   });  
-// })
+ 
